cd238279a773454476997eb2fba38008c5e99c38
Shults Pavel <bmstustudent@yandex.ru>
2021-01-09 23:52:19 +0300

    fix: 1.0.1

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 3000;
-const path = require('path');
-const usersRoutes = require('./routes/users.js');
-const cardsRoutes = require('./routes/cards.js');
+const path = require("path");
+const usersRoutes = require("./routes/users.js");
+const cardsRoutes = require("./routes/cards.js");
 
 //раздаём статические файлы
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //JSON-список всех пользователей
-app.use('/users', usersRoutes);
+app.use("/users", usersRoutes);
 //JSON-список всех карточек
-app.use('/cards', cardsRoutes);
+app.use("/cards", cardsRoutes);
 
 //Несуществующий адрес
-app.all('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+app.all("*", (req, res) =>
+  res.status(404).send({ message: "Запрашиваемый ресурс не найден" })
+);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
