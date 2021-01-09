afb575d276069e7a162a724dfd390e33ca95ee8f
Shults Pavel <bmstustudent@yandex.ru>
2021-01-09 21:15:11 +0300

    feat:1.0.0

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+const path = require('path');
+const usersRoutes = require('./routes/users.js');
+const cardsRoutes = require('./routes/cards.js');
+
+//раздаём статические файлы
+app.use(express.static(path.join(__dirname, 'public')));
+//JSON-список всех пользователей
+app.use('/users', usersRoutes);
+//JSON-список всех карточек
+app.use('/cards', cardsRoutes);
+
+//Несуществующий адрес
+app.all('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
