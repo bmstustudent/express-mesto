b409354b24e429c43501be3aa246a39698405532
Shults Pavel <bmstustudent@yandex.ru>
2021-01-10 15:33:39 +0300

    fix: 0 errors ESLint

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const PORT = 3000;
-const path = require("path");
-const usersRoutes = require("./routes/users.js");
-const cardsRoutes = require("./routes/cards.js");
+const path = require('path');
+const usersRoutes = require('./routes/users.js');
+const cardsRoutes = require('./routes/cards.js');
 
-//раздаём статические файлы
-app.use(express.static(path.join(__dirname, "public")));
-//JSON-список всех пользователей
-app.use("/users", usersRoutes);
-//JSON-список всех карточек
-app.use("/cards", cardsRoutes);
+//  раздаём статические файлы
+app.use(express.static(path.join(__dirname, 'public')));
+// JSON-список всех пользователей
+app.use('/users', usersRoutes);
+// JSON-список всех карточек
+app.use('/cards', cardsRoutes);
 
-//Несуществующий адрес
-app.all("*", (req, res) =>
-  res.status(404).send({ message: "Запрашиваемый ресурс не найден" })
-);
+// Несуществующий адрес
+app.all('*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
-
-//не совсем понял "ошибка линтера", как именно это посмотреть, ESLint установлен, 3 зависимости установлены.
